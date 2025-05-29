ddd26cbd07640218f4f411c250917c305ec2c398
dzimiks <vana997@gmail.com>
2025-05-29 12:14:36 +0200

    chore: add Plausible analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 // src/app/layout.tsx
 import type { Metadata } from 'next';
+import PlausibleProvider from 'next-plausible';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { PrivyClientProvider } from '@/components/PrivyProvider';
@@ -17,12 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <PrivyClientProvider>
-          {children}
-        </PrivyClientProvider>
-      </body>
-    </html>
+    <PlausibleProvider domain="onebalance-privy-demo.vercel.app">
+      <html lang="en">
+        <body className={inter.className}>
+          <PrivyClientProvider>
+            {children}
+          </PrivyClientProvider>
+        </body>
+      </html>
+    </PlausibleProvider>
   );
 }
